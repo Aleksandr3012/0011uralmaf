1de25eb2a02e8c9106cd86dcfbcd5ac047c310a3
Pavel <wol1414@gmail.com>
2020-04-19 13:32:16 +0300

    edit padding

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -158,7 +158,7 @@ function eventHandler() {
 
 	// JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
-	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/01-320.png);"></div>');
+	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/011.png);"></div>');
 	// /добавляет подложку для pixel perfect
 
 
